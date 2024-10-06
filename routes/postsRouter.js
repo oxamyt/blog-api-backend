602c0f5138281b78fd1d47f4c84a0451fac99589
Oxamyt <dentez.danil@gmail.com>
2024-10-06 14:52:12 +0300

    fix jwt-authenticate and implement passport-jwt

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -3,15 +3,35 @@ const postsController = require("../controllers/postsController");
 const commentsRouter = require("./commentsRouter");
 const passport = require("../middlewares/passportConfig");
 const isAdmin = require("../middlewares/isAdmin");
+const authenticateJWT = require("../middlewares/authJWT");
 
 const postsRouter = Router({ mergeParams: true });
 
-postsRouter.get("/", postsController.fetchPosts);
-postsRouter.get("/:id", postsController.fetchSinglePost);
+postsRouter.get("/", passport.authenticate("jwt"), postsController.fetchPosts);
+postsRouter.get(
+  "/:id",
+  passport.authenticate("jwt"),
+  postsController.fetchSinglePost
+);
 
-postsRouter.post("/", isAdmin, postsController.createPost);
-postsRouter.put("/:id", isAdmin, postsController.editPost);
-postsRouter.delete("/:id", isAdmin, postsController.deletePost);
+postsRouter.post(
+  "/",
+  passport.authenticate("jwt"),
+  isAdmin,
+  postsController.createPost
+);
+postsRouter.put(
+  "/:id",
+  passport.authenticate("jwt"),
+  isAdmin,
+  postsController.editPost
+);
+postsRouter.delete(
+  "/:id",
+  passport.authenticate("jwt"),
+  isAdmin,
+  postsController.deletePost
+);
 
 postsRouter.use("/:id/comments", commentsRouter);
 
